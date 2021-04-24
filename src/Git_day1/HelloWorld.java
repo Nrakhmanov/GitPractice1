6b044b974b8be18b2376b8c9b2c8c70aac6da148
Nuriddin Rakhmanov <nuriddin.rakhmanov@gmail.com>
2021-04-24 14:42:56 -0600

    added one more line of code

--- src/Git_day1/HelloWorld.java
+++ src/Git_day1/HelloWorld.java
@@ -3,5 +3,6 @@ package Git_day1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Adding Github now");
     }
 }
